9ec9224a854a17dd01a112d891c3ed19f3319db6
Your Name <patlollaa3@udayton.edu>
2024-11-10 20:48:10 -0500

    kyc table

---
+++ kyc.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `kyc` (
+   `GovtId` varchar(20) NOT NULL,
+   `FirstName` varchar(60) NOT NULL,
+   `LastName` varchar(60) NOT NULL,
+   `DOB Date` varchar(45) NOT NULL,
+   `Nationality` varchar(50) DEFAULT NULL,
+   `Gender` varchar(10) DEFAULT NULL,
+   PRIMARY KEY (`GovtId`),
+   UNIQUE KEY `GovtId_UNIQUE` (`GovtId`)
+ )
