7ac098ab583ac0638028de7cdb1608afe9ec9d2c
Your Name <patlollaa3@udayton.edu>
2024-11-25 00:38:00 -0500

    ALL

--- bank.sql
+++ bank.sql
@@ -1,5 +1,5 @@
-CREATE TABLE bank (
+CREATE TABLE BANK (
    ISFC varchar(20) PRIMARY KEY,
-   bankName varchar(100) NOT NULL,
-   ZipCode varchar(10) NOT NULL
+   BANKNAME varchar(100) NOT NULL,
+   ZIPCODE varchar(10) NOT NULL
  );
